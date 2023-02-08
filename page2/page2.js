50788d2b2e4259d1ad406f9159409dbd8080ed1b
Anna Danilova <112716383+danilovabg@users.noreply.github.com>
2023-02-08 12:15:38 +0200

    Update page2.js

--- page2/page2.js
+++ page2/page2.js
@@ -1,77 +1,11 @@
+const response = await fetch("https://danilovabg-detection-project.hf.space/run/predict", {
+	method: "POST",
+	headers: { "Content-Type": "application/json" },
+	body: JSON.stringify({
+		data: [
+			"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACklEQVR4nGMAAQAABQABDQottAAAAABJRU5ErkJggg==",
+		]
+	})
+});
 
-(() => {
-    'use strict';
-    const classificationEndpoint = "https://danilovabg-detection-project.hf.space/run/predict/";
-    const resultsElement = document.querySelector('.result');
-
-    /**
-     * Clear previous results
-     */
-    function resetResultsText() {
-        resultsElement.innerHTML = '';
-    }
-
-    /**
-     * Output results on the page
-     * @param {Object} result
-     */
-    function outputResults(result) {
-        
-        let resultHTML;
-
-        result.forEach((element) => {
-            resultHTML = `<hr> ${element.label} <br><br>`;
-
-            if (element.confidences) {
-                element.confidences.forEach((element) => {
-                    resultHTML += `${element}<br>`;
-                });
-            }
-        });
-
-        resultsElement.innerHTML = resultHTML;
-    }
-
-    /**
-     * Convert file to base64
-     * @param {Node} fileInput
-     * @returns {Promise<void>}
-     */
-    async function fileInputToBase64 (fileInput) {
-        debugger;
-        return new Promise((resolve, reject) => {
-            const fileReader = new FileReader();
-            fileReader.readAsDataURL(fileInput.files[0]);
-            fileReader.onload = () => resolve(fileReader.result);
-            fileReader.onerror = (error) => reject(error);
-        });
-    }
-
-    /**
-     * Handle form submission
-     * @param {Object} formData
-     */
-    async function formSubmit(formData) {
-        fetch(classificationEndpoint, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(formData),
-        })
-            .then(res => res.json())
-            .then(res => outputResults(res.data))
-    }
-
-    document.querySelector('form').addEventListener('submit', async (e) => {
-        e.stopPropagation();
-        e.preventDefault();
-        resetResultsText();
-
-        const fileInput = e.target.querySelector('input[type="file"]');
-        const encodedFile = await fileInputToBase64(fileInput);
-        const formData = {
-            data: [ encodedFile ]
-        };
-
-        await formSubmit(formData);
-    });
-})(); 
+const data = await data.json();
